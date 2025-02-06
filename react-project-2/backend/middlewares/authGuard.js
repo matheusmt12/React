73b9e35df39e6a9c2fba24e89fae865c3fb5f240
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2025-02-06 14:35:52 -0300

    a

---
+++ react-project-2/backend/middlewares/authGuard.js
@@ -0,0 +1,30 @@
+const User = require("../model/User");
+
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const authGuard = async (req, res, next) =>{
+
+    const autHeader = req.headers['authorization'];
+    const token = autHeader && autHeader.split(" ")[1];
+
+    //verificar token
+
+    if(!token) res.status(401).json({errors : ['Acesso negado!']});
+    
+
+    try {
+        const verified = jwt.verify(token, jwtSecret);
+
+        req.user = await User.findById(verified.id).select("-password");
+    
+        res.status(200).json(req.user);
+    } catch (error) {
+        res.status(401).json({errors : ['Token inválido!']})    
+    }
+
+
+}
+
+module.exports = authGuard;
